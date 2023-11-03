3f686570580085f69892e73f489180c1751ad26f
jkic2023github <149748656+jkic2023github@users.noreply.github.com>
2023-11-03 13:38:12 -0400

    Changed script and added image 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the logo element by its ID
     const logo = document.getElementById('logo');
+    const images = ['profile.jpg', 'profile1.jpg']; // Array of image sources
+    let currentIndex = 0; // Initial index to display the first image
 
-    // Add a CSS class to initiate the spinning animation
-    logo.classList.add('spin-animation');
+    function toggleImage() {
+        logo.src = images[currentIndex];
+        currentIndex = (currentIndex + 1) % images.length; // Toggle between 0 and 1
+    }
+
+    setInterval(toggleImage, 2000); // Change the image every 2 seconds (2000 milliseconds)
 });
